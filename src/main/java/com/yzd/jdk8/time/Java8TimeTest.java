34dc28c2eb51519e1d468c2e840da5fd78dc4357
yanzhidong <yanzhidong@sm.vvip-u.com>
2019-10-24 15:55:59 +0800

    java8Time

--- src/main/java/com/yzd/jdk8/time/Java8TimeTest.java
+++ src/main/java/com/yzd/jdk8/time/Java8TimeTest.java
@@ -1,8 +1,8 @@
 package com.yzd.jdk8.time;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.MonthDay;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /***
  *
@@ -27,8 +27,40 @@ public class Java8TimeTest {
         System.out.println("----------------");
         MonthDay from = MonthDay.from(LocalDate.of(2019, 11, 23));
         System.out.println(from);
-
+        System.out.println("----------------");
         LocalTime localTime = LocalTime.now();
         System.out.println(localTime);
+        System.out.println(localTime.minusHours(1));
+        System.out.println("----------------");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+        System.out.println("----------------");
+        System.out.println(ZoneId.getAvailableZoneIds());
+
+        System.out.println("----------------");
+        DateTimeFormatter ofPattern = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+        System.out.println(LocalDateTime.now().format(ofPattern));
+
+        System.out.println("----------------");
+        System.out.println(toDate(LocalDateTime.now()));
+        System.out.println(toLocalDateTime(new Date()));
+    }
+
+    /**
+     * LocalDateTime -> Date
+     */
+    public static Date toDate(LocalDateTime localDateTime) {
+        ZoneId zoneId = ZoneId.systemDefault();
+        ZonedDateTime zdt = localDateTime.atZone(zoneId);
+        return Date.from(zdt.toInstant());
+    }
+
+    /**
+     * Date -> LocalDateTime
+     */
+    public static LocalDateTime toLocalDateTime(Date date) {
+        Instant instant = date.toInstant();
+        ZoneId zoneId = ZoneId.systemDefault();
+        return instant.atZone(zoneId).toLocalDateTime();
     }
 }
